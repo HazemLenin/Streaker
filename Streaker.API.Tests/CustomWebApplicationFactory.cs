975365dace546fcdeb3cde64f632194465e50525
Hazem Lenin <hazemlenin@gmail.com>
2024-01-20 20:27:41 +0200

    setup tests project

---
+++ Streaker.API.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Streaker.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streaker.API.Tests
+{
+    public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (dbContextDescriptor != null)
+                    services.Remove(dbContextDescriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbConnection));
+
+                if (dbConnectionDescriptor != null)
+                    services.Remove(dbConnectionDescriptor);
+
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<ApplicationDbContext>((container, options) =>
+                {
+                    options.UseInMemoryDatabase(new Guid().ToString());
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
---
+++ Streaker.API.Tests/Streaks/StreaksTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Streaker.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streaker.API.Tests.Streaks
+{
+    public class StreaksTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly CustomWebApplicationFactory<Program>
+        _factory;
+
+        public StreaksTests(
+            CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+
+            //using var scope = factory.Services.CreateScope();
+            //var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            //context.Streaks.Add(new()
+            //{
+            //    Id = "1",
+            //    Category = "Sports",
+            //    Created = DateTime.UtcNow,
+            //    Name = "streak 1",
+            //    TargetCount = 60,
+            //});
+            //context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task Get_CheckStreaks()
+        {
+            //// Arrange
+            //var client = _factory.CreateClient();
+
+            //// Act
+            //var response = await client.GetAsync("/api/Streaks");
+
+            //// Assert
+            //response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            //var streaks = await response.Content.ReadFromJsonAsync<List<StreakDto>>(); // Assuming StreakDto is your DTO class
+
+            //Assert.NotNull(streaks);
+            //Assert.Single(streaks); // Assuming you expect only one streak in the response
+
+            //var expectedStreak = streaks.First(); // Assuming you are expecting the first streak
+            //Assert.Equal("1", expectedStreak.Id);
+            //Assert.Equal("Sports", expectedStreak.Category);
+            //Assert.Equal("streak 1", expectedStreak.Name);
+            //Assert.Equal(60, expectedStreak.TargetCount);
+            Assert.Equal(1, 1);
+        }
+    }
+}
