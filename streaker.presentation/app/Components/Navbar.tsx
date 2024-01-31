cfd818ddcc4e1288fd8346ffad1e825f416ab499
Hazem Lenin <hazemlenin@gmail.com>
2024-01-31 18:51:01 +0200

    middleware setup

--- streaker.presentation/app/Components/Navbar.tsx
+++ streaker.presentation/app/Components/Navbar.tsx
@@ -11,21 +11,32 @@ import {
 import { useSelector } from "react-redux";
 import { IRootState } from "../store";
 import { useEffect, useState } from "react";
+import { Pacifico } from "next/font/google";
+
+const pacifico = Pacifico({ weight: ["400"], subsets: ["latin"] });
 
 export default function Navbar() {
 	const tokens = useSelector((state: IRootState) => state.tokens);
 
 	return (
 		<nav className="flex items-center justify-between px-3 mb-10 h-16 border-b border-muted shadow-lg">
-			<ul className="flex gap-10">
+			<ul className="flex items-center gap-10">
 				<li>
-					<Link href="/">
+					<Link href="/" className="flex gap-2 items-center">
 						<img src={logo.src} className="w-10" />
+						<p className={`hidden md:inline ${pacifico.className}`}>Streaks</p>
 					</Link>
 				</li>
+				{!!tokens && (
+					<>
+						<li>
+							<Link href="/streakers">Streakers</Link>
+						</li>
+					</>
+				)}
 			</ul>
 			{tokens ? (
-				<ul className="flex gap-10">
+				<ul className="flex items-center gap-10">
 					<li>
 						<Link href="/logout">
 							Logout <FontAwesomeIcon icon={faSignOut} />
---
+++ streaker.presentation/app/streakers/page.tsx
@@ -0,0 +1,3 @@
+export default function Streakers() {
+	return <h1 className="text-4xl font-bold">Streakers page working!!!</h1>;
+}
